4719b18377a73accf06f93ce32405639416db6e1
suryauppalapati <suryauppalapati9@gmail.com>
2023-11-10 10:30:10 +0530

    gsuite code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
-function App() {
+const App = () => {
+  const [labels, setLabels] = useState([]);
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  const responseGoogle = (response) => {
+    if (response.profileObj) {
+      setIsAuthorized(true);
+      listLabels();
+    }
+  };
+
+  const logout = () => {
+    setIsAuthorized(false);
+    setLabels([]);
+  };
+
+  const listLabels = async () => {
+    try {
+      const response = await window.gapi.client.gmail.users.labels.list({
+        userId: "me",
+      });
+      const labels = response.result.labels || [];
+      const labelNames = labels.map((label) => label.name);
+      setLabels(labelNames);
+    } catch (error) {
+      console.error("Error listing labels:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Gmail API Quickstart</h1>
+      {isAuthorized ? (
+        <div>
+          <GoogleLogout
+            clientId='923881872905-t8cmtnlmccck4vpi5cuhtv5rvci3q0dc.apps.googleusercontent.com'
+            buttonText='Logout'
+            onLogoutSuccess={logout}
+            onFailure={logout}
+          />
+          <pre>{JSON.stringify(labels, null, 2)}</pre>
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId='923881872905-t8cmtnlmccck4vpi5cuhtv5rvci3q0dc.apps.googleusercontent.com'
+          buttonText='Login'
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
